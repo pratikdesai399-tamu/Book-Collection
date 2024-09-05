48490de15f8ba3784b4f7b0c4b81175187e9dda3
Rishi Santhanam <rishisant@tamu.edu>
2024-09-05 15:55:13 -0500

    [ADD] All Files

---
+++ db/migrate/20240901181201_add_attributes_to_book.rb
@@ -0,0 +1,7 @@
+class AddAttributesToBook < ActiveRecord::Migration[7.0]
+  def change
+    add_column :books, :author, :string
+    add_column :books, :price, :decimal
+    add_column :books, :published, :date
+  end
+end
